f0b197eb1dd55c80deca749df3b493c4e9bf587d
Gabriele Canepa <gabriele@canepa.io>
2018-05-28 23:25:22 +0100

    Add Restaurants#top action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # New general and not CRUD urls must be nested with the collection method at a collection level, not a member level (/:id/new-url)
+    collection do
+      get 'top' => 'restaurants#top' # will generate a route with top_restaurants as prefix and /restaurants/top as URI pattern
+    end
+  end
 end
